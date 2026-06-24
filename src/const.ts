52e1f3f90ffe62813349bb7435792f6c913dd7ac
kkarinzzz <maoukarin@gmail.com>
2026-06-24 17:07:03 +0700

    footer

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,8 @@
 export const NXCTF = {
   nxctf_title: 'NXCTF',
-  nxctf_url: 'https://nxctf.my.id',
+  nxctf_url: 'https://ktcg.my.id',
   nxctf_logo: 'https://raw.githubusercontent.com/nxctf/assets/refs/heads/main/logo/logo-no-bg.svg',
-  nxctf_discord: 'https://discord.gg/5etKks6aQQ',
+  nxctf_discord: 'https://discord.gg/Q5dVVBs6xX',
   nxctf_donation: 'https://saweria.co/nxctf',
   nxctf_github: 'https://github.com/nxctf/nxctf',
   nxctf_github_org: 'https://github.com/nxctf',
